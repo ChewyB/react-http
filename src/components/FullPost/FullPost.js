f418d4b1df9db2e9805cf7d0ea841d982f8a137e
ChewyB <Jb515764>
2018-10-30 18:18:25 -0700

    Finished mini lecture on post,get,delete using axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -19,26 +19,18 @@ class FullPost extends Component {
           .then(response => {
             //console.log(response);
             this.setState({ loadedPost: response.data });
-          });
+          })
+          
       }
     }
   }
 
-  //   componentDidUpdate() {
-  //     if (this.props.id) {
-  //       if (
-  //         !this.state.loadedPost ||
-  //         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
-  //       ) {
-  //         axios
-  //           .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-  //           .then(response => {
-  //             // console.log(response);
-  //             this.setState({ loadedPost: response.data });
-  //           });
-  //       }
-  //     }
-  //   }
+  deletePostHandler = () => {
+    axios.delete("https://jsonplaceholder.typicode.com/posts/"+ this.props.id)
+          .then(response => {
+            //console.log(response);
+          });
+  }
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
@@ -51,7 +43,7 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
       );
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,51 +3,64 @@ import React, { Component } from "react";
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
-import axios from "axios";
+//import axios from "axios";
+import axios from '../../axios';
 import "./Blog.css";
 
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
-  }
+    selectedPostId: null,
+    error: false
+  };
 
   componentDidMount() {
-    var url = "https://jsonplaceholder.typicode.com/posts";
-    axios.get(url)
-         .then(response => {
-           const posts = response.data.slice(0,4);
-           const updatedPosts = posts.map(post => {
-             return {
-               ...post,
-               author: 'Max'
-             }
-           })
-             this.setState({posts: updatedPosts})
-            console.log(response);
+    var url = "/posts";
+    axios
+      .get(url)
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
         });
+
+        this.setState({ posts: updatedPosts });
+        //console.log(response);
+      })
+      .catch(error => {
+        //console.log(error);
+        this.setState({ error: true });
+      });
   }
 
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id})
-  }
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
 
   render() {
-    const posts = this.state.posts.map(post => {
-        return <Post title={post.title} 
-                     key={post.id} 
-                     author={post.author}
-                     clicked={() => this.postSelectedHandler(post.id)}></Post>
-    })
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            title={post.title}
+            key={post.id}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
 
     return (
       <div>
-        <section className="Posts">
-        {posts}
-        </section>
-        
+        <section className="Posts">{posts}</section>
+
         <section>
-          <FullPost id={this.state.selectedPostId}/>
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
